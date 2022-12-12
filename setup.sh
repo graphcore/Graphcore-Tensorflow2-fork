7eb2c35f41c9ff68794bb7c9445259a0cce3bbbe
Alexandre Payot <alexandrep@graphcore.ai>
2022-12-12 11:04:34 +0000

    Test from the runtime repository

--- setup.sh
+++ setup.sh
@@ -2,24 +2,12 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 # Script to be sourced on launch of the Gradient Notebook
 
-# called from root folder in container
-symlink-public-resources() {
-    public_source_dir=${1}
-    target_dir=${2}
-    echo "Symlinking - ${public_source_dir} to ${target_dir}"
-
-    # Make sure it exists otherwise you'll copy your current dir
-    mkdir -p ${public_source_dir}
-    cd ${public_source_dir}
-    find -type d -exec mkdir -p "${target_dir}/{}" \;
-    #find -type f -not -name "*.lock" -exec cp -sP "${PWD}/{}" "${target_dir}/{}" \;
-    find -type f -not -name "*.lock" -print0 | xargs -0 -P 50 -I {} sh -c "cp -sP \"${PWD}/{}\" \"${target_dir}/{}\""
-    cd -
-}
-
-DETECTED_NUMBER_OF_IPUS="4"
-
-IPU_ARG=${1:-"${DETECTED_NUMBER_OF_IPUS}"}
+DETECTED_NUMBER_OF_IPUS=$(python .gradient/available_ipus.py)
+if [[ "$1" == "test" ]]; then
+    IPU_ARG="${DETECTED_NUMBER_OF_IPUS}"
+else
+    IPU_ARG=${1:-"${DETECTED_NUMBER_OF_IPUS}"}
+fi
 
 export NUM_AVAILABLE_IPU=${IPU_ARG}
 export GRAPHCORE_POD_TYPE="pod${IPU_ARG}"
@@ -39,23 +27,10 @@ export OGB_DATASET_DIR="${DATASET_DIR}/ogb_lsc_pcqm4mv2/datasets"
 export OGB_CHECKPOINT_DIR="${CHECKPOINT_DIR}/ogb_lsc_pcqm4mv2/checkpoints"
 export OGB_SUBMISSION_CODE="./pcqm4mv2_submission"
 
-make_ogb_custom_ops(){
-    python -m pip install -r /notebooks/ogb-competition/requirements.txt
-    cd "/notebooks/ogb-competition/${OGB_SUBMISSION_CODE}"
-    make -C data_utils/feature_generation
-    make -C static_ops
-    cd -
-}
 
-prepare_tf2_datasets(){
-    # symlink exe_cache files
-    symlink-public-resources "${PUBLIC_DATASET_DIR}/exe_cache" $POPLAR_EXECUTABLE_CACHE_DIR
-    # symlink ogbn_arxiv dataset for cluster gcn notebook
-    symlink-public-resources "${PUBLIC_DATASET_DIR}/datasets/ogbn_arxiv" "${DATASET_DIR}/ogbn_arxiv"
-    # symlink OGB-specific folders
-    symlink-public-resources "${PUBLIC_DATASET_DIR}/datasets/ogb_lsc_pcqm4mv2/datasets" "${OGB_DATASET_DIR}"
-    symlink-public-resources "${PUBLIC_DATASET_DIR}/datasets/ogb_lsc_pcqm4mv2/checkpoints" "${OGB_CHECKPOINT_DIR}"
-    make_ogb_custom_ops
-}
+nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
 
-nohup prepare_datasets & tail -f nohup.out &
+export PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp
+jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
+            --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
+            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
