2ad3a0412033a04c281528bfb2aa1b3b92b7552f
Rahul Trada <rahult@graphcore.ai>
2023-03-13 11:27:44 +0000

    use python and config json file for symlinking

--- .gradient/prepare-datasets.sh
+++ .gradient/prepare-datasets.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
-
-symlink-public-resources() {
-    public_source_dir=${1}
-    target_dir=${2}
-
-    # need to wait until the dataset has been mounted (async on Paperspace's end)
-    while [ ! -d ${public_source_dir} ] || [ -z "$(ls -A ${public_source_dir})" ]
-    do
-        echo "Waiting for dataset "${public_source_dir}" to be mounted..."
-        sleep 1
-    done
-
-    echo "Symlinking - ${public_source_dir} to ${target_dir}"
-
-    # Make sure it exists otherwise you'll copy your current dir
-    mkdir -p ${target_dir}
-    workdir="/fusedoverlay/workdirs/${public_source_dir}"
-    upperdir="/fusedoverlay/upperdir/${public_source_dir}"
-    mkdir -p ${workdir}
-    mkdir -p ${upperdir}
-    fuse-overlayfs -o lowerdir=${public_source_dir},upperdir=${upperdir},workdir=${workdir} ${target_dir}
-
-}
+#! /usr/bin/env bash 
+set -u 
+set -o pipefail
 
 if [ ! "$(command -v fuse-overlayfs)" ]
 then
@@ -30,15 +9,11 @@ then
     apt install -o DPkg::Lock::Timeout=120 -y psmisc libfuse3-dev fuse-overlayfs
 fi
 
-echo "Starting preparation of datasets"
 
-# symlink exe_cache files
-symlink-public-resources "${PUBLIC_DATASET_DIR}/poplar-executables-tf2-3-1" $POPLAR_EXECUTABLE_CACHE_DIR
-# symlink ogbn_arxiv dataset for cluster gcn notebook
-symlink-public-resources "${PUBLIC_DATASET_DIR}/ogbn_arxiv" "${DATASET_DIR}/ogbn_arxiv"
-# symlink OGB-specific folders
-symlink-public-resources "${PUBLIC_DATASET_DIR}/ogb_lsc_pcqm4mv2/datasets" "${OGB_DATASET_DIR}"
-symlink-public-resources "${PUBLIC_DATASET_DIR}/ogb_lsc_pcqm4mv2/checkpoints" "${OGB_CHECKPOINT_DIR}"
+echo "Starting preparation of datasets"
+cd "$(dirname "$0")"
+python -u ./symlink_datasets_and_caches.py
+cd -
 
 # Make the custom ops for the OGB notebooks
 python -m pip install -r /notebooks/ogb-competition/requirements.txt
@@ -50,9 +25,8 @@ cd -
 
 echo "Finished running setup.sh."
 # Run automated test if specified
-if [[ "$1" == "test" ]]; then
-
+if [[ "${1:-}" == "test" ]]; then
     bash /notebooks/.gradient/automated-test.sh "${@:2}"
-elif [[ "$2" == "test" ]]; then
+elif [[ "${2:-}" == "test" ]]; then
     bash /notebooks/.gradient/automated-test.sh "${@:3}"
 fi
